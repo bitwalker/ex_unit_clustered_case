c37a3d4a1ca34ff26cafdd5267b03594be6f1200
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2019-06-26 20:53:52 -0400

    A few small refactorings

--- lib/app.ex
+++ lib/app.ex
@@ -4,31 +4,25 @@ defmodule ExUnit.ClusteredCase.App do
 
   def start(_type, _args) do
     children =
-      case in_cluster_node?() do
-        true ->
-          []
+      if is_clustered_node?() do
+        # Do not start boot server/children if running as a cluster node
+        []
+      else
+        # We depend on the boot server, so start it if not started yet
+        unless Process.whereis(:boot_server) do
+          {:ok, _} = :erl_boot_server.start_link([{127, 0, 0, 1}])
+        end
 
-        false ->
-          # We depend on the boot server, so start it if not started yet
-          unless Process.whereis(:boot_server) do
-            {:ok, _} = :erl_boot_server.start_link([{127, 0, 0, 1}])
-          end
-
-          [
-            {ExUnit.ClusteredCase.Node.Ports, []},
-            {ExUnit.ClusteredCase.Cluster.Supervisor, []}
-          ]
+        [
+          {ExUnit.ClusteredCase.Node.Ports, []},
+          {ExUnit.ClusteredCase.Cluster.Supervisor, []}
+        ]
       end
 
     Supervisor.start_link(children, strategy: :one_for_one)
   end
 
-  defp in_cluster_node? do
-    node_name() =~ "ex_unit_clustered_node_"
-  end
-
-  defp node_name do
-    Node.self()
-    |> Atom.to_string()
+  defp is_clustered_node? do
+    Atom.to_string(node()) =~ "ex_unit_clustered_node_"
   end
 end
