905342fa7624a52ff00f30a351f8ce378514ea01
Matteo <mbrancaleoni@gmail.com>
2019-06-27 02:54:20 +0200

    Start erl_boot_server only in primary nodes (#7)

--- lib/app.ex
+++ lib/app.ex
@@ -3,16 +3,26 @@ defmodule ExUnit.ClusteredCase.App do
   use Application
 
   def start(_type, _args) do
-    # We depend on the boot server, so start it if not started yet
-    unless Process.whereis(:boot_server) do
-      {:ok, _} = :erl_boot_server.start_link([{127, 0, 0, 1}])
-    end
+    children =
+      if is_clustered_node?() do
+        # Do not start boot server/children if running as a cluster node
+        []
+      else
+        # We depend on the boot server, so start it if not started yet
+        unless Process.whereis(:boot_server) do
+          {:ok, _} = :erl_boot_server.start_link([{127, 0, 0, 1}])
+        end
 
-    children = [
-      {ExUnit.ClusteredCase.Node.Ports, []},
-      {ExUnit.ClusteredCase.Cluster.Supervisor, []}
-    ]
+        [
+          {ExUnit.ClusteredCase.Node.Ports, []},
+          {ExUnit.ClusteredCase.Cluster.Supervisor, []}
+        ]
+      end
 
     Supervisor.start_link(children, strategy: :one_for_one)
+  end
+
+  defp is_clustered_node? do
+    Atom.to_string(node()) =~ "ex_unit_clustered_node_"
   end
 end
